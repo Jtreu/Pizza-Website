4bd4c498d0073762d0fe671339858bd5f11976d7
Jameson Treu <jamesontreu@gmail.com>
2016-09-07 17:04:36 -0500

    Add files via upload

---
+++ js/document.js
@@ -0,0 +1,110 @@
+var doc = {};
+
+/* GUI */
+
+doc["blackScreen"] = document.getElementById( "blackScreen" );
+doc["checkoutCart"] = document.getElementById( "checkoutCart" );
+doc["onSuccessDiv"] = document.getElementById( "onSuccessDiv" );
+doc["placeOrderBtn"] = document.getElementById( "placeOrderBtn" );
+doc["pOrderTime"] = document.getElementById( "pOrderTime" );
+doc["pCart"] = document.getElementById( "pCart" );
+
+/* PIZZA STUFF */
+
+
+doc["price"] = document.getElementById( "price" );
+doc["pizzaCounter"] = document.getElementById( "pizzaCounter" );
+
+/* PIZZA PLATFORM */
+doc["pizzaPlatform"] = document.getElementById( "pizzaPlatform" );
+doc["pToppings"] = document.getElementById( "pToppings" );
+doc["pPaddle"] = document.getElementById( "pPaddle" );
+
+/* STYLE */
+doc["pStyle"] = document.getElementById( "pStyle" );
+doc["pStyle-arrow"] = document.getElementById( "pStyle-arrow" );
+
+
+doc["pSizeLabel"] = document.getElementById( "pSizeLabel" );
+doc["pSlicesLabel"] = document.getElementById( "pSlicesLabel" );
+
+doc["circle"] = document.getElementById( "circle" );
+doc["square"] = document.getElementById( "square" );
+doc["heart"] = document.getElementById( "heart" );
+doc["batman"] = document.getElementById( "batman" );
+
+doc["radial"] = document.getElementById( "radial" );
+doc["grid"] = document.getElementById( "grid" );
+
+/* DOUGH */
+doc["pDough"] = document.getElementById( "pDough" );
+doc["pDough-arrow"] = document.getElementById( "pDough-arrow" );
+doc["pita"] = document.getElementById( "pita" );
+doc["gFree"] = document.getElementById( "gFree" );
+doc["vegan"] = document.getElementById( "vegan" );
+doc["regular"] = document.getElementById( "regular" );
+
+
+/* SAUCE */
+doc["pSauce"] = document.getElementById( "pSauce" );
+doc["pSauce-arrow"] = document.getElementById( "pSauce-arrow" );
+doc["pesto"] = document.getElementById( "pesto" );
+doc["salsa"] = document.getElementById( "salsa" );
+doc["bbq"] = document.getElementById( "bbq" );
+doc["ranchy"] = document.getElementById( "ranchy" );
+doc["romesco"] = document.getElementById( "romesco" );
+doc["sunDried"] = document.getElementById( "sunDried" );
+doc["nutella"] = document.getElementById( "nutella" );
+
+/* CHEESE */
+doc["pCheese"] = document.getElementById( "pCheese" );
+doc["pCheese-arrow"] = document.getElementById( "pCheese-arrow" );
+doc["provolone"] = document.getElementById( "provolone" );
+doc["cheddar"] = document.getElementById( "cheddar" );
+doc["colby"] = document.getElementById( "colby" );
+doc["mozzarella"] = document.getElementById( "mozzarella" );
+doc["creamCheese"] = document.getElementById( "creamCheese" );
+doc["vMozzarella"] = document.getElementById( "vMozzarella" );
+doc["vCreamCheese"] = document.getElementById( "vCreamCheese" );
+doc["cashewCheese"] = document.getElementById( "cashewCheese" );
+
+
+/* MEATS */
+doc["pMeats"] = document.getElementById( "pMeats" );
+doc["pMeats-arrow"] = document.getElementById( "pMeats-arrow" );
+doc["pepperoni"] = document.getElementById( "pepperoni" );
+doc["bacon"] = document.getElementById( "bacon" );
+doc["chicken"] = document.getElementById( "chicken" );
+doc["beef"] = document.getElementById( "beef" );
+doc["sausage"] = document.getElementById( "sausage" );
+doc["turkey"] = document.getElementById( "turkey" );
+doc["tofurkey"] = document.getElementById( "tofurkey" );
+doc["tofu"] = document.getElementById( "tofu" );
+doc["vBeef"] = document.getElementById( "vBeef" );
+doc["vBacon"] = document.getElementById( "vBacon" );
+doc["vChicken"] = document.getElementById( "vChicken" );
+
+/* VEGETABLES */
+doc["pVegetables"] = document.getElementById( "pVegetables" );
+doc["pVegetables-arrow"] = document.getElementById( "pVegetables-arrow" );
+doc["spinach"] = document.getElementById( "spinach" );
+doc["mushrooms"] = document.getElementById( "mushrooms" );
+doc["artichoke"] = document.getElementById( "artichoke" );
+doc["cucumbers"] = document.getElementById( "cucumbers" );
+doc["onions"] = document.getElementById( "onions" );
+doc["squanchy"] = document.getElementById( "squanchy" );
+doc["tomatoes"] = document.getElementById( "tomatoes" );
+
+/* FRUITS */
+doc["pFruits"] = document.getElementById( "pFruits" );
+doc["pFruits-arrow"] = document.getElementById( "pFruits-arrow" );
+doc["apples"] = document.getElementById( "apples" );
+doc["pineapples"] = document.getElementById( "pineapples" );
+doc["grapes"] = document.getElementById( "grapes" );
+doc["bananas"] = document.getElementById( "bananas" );
+doc["oranges"] = document.getElementById( "oranges" );
+doc["mangoes"] = document.getElementById( "mangoes" );
+doc["strawberries"] = document.getElementById( "strawberries" );
+
+/* TESTING */
+doc["status"] = document.getElementById("status");
